9be1af5207dc251d4a8835c0f1766474736ab8e8
hyungeunjo8 <hyungeunjo@weversecompany.com>
2022-08-12 15:34:26 +0900

    init commit

---
+++ src/main/java/com/example/ekspractice/EksPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ekspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ekspractice/EksPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ekspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
